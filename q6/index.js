e3a3f2d3bbe3f11c01646f685383e320be7a8fad
shristabaruah <eternalshrista@gmail.com>
2021-10-01 20:45:02 +0530

    complete

---
+++ q6/index.js
@@ -0,0 +1,6 @@
+const loadingDiv = document.querySelector(".loading")
+const loadedButton = document.querySelector(".loaded")
+
+loadedButton.addEventListener("click",()=>{
+    loadingDiv.style.display='none'
+})
